45fdf5d73324f9d7e131da6222523dde08dbbe71
Rizhao <rizhao.cai@gmail.com>
2022-06-22 14:01:39 +0800

    Refactor the extract_frame_from_video.py

--- preliminary/extract_frame_from_video.py
+++ preliminary/extract_frame_from_video.py
@@ -20,9 +20,36 @@ import os
 import numpy as np
 import pdb
 
-detector = MtcnnDetector(model_folder='mxnet_mtcnn_face_detection/model', ctx=mx.cpu(0), num_worker=8,
+
+# ==================================Build detector =========================================================
+
+detector = MtcnnDetector(model_folder='mxnet_mtcnn_face_detection/model', ctx=mx.cpu(0), num_worker=0,
                          accurate_landmark=False)
 
+def detect_face_and_save_landmarks(image_name, image_frame=None):
+    global detector
+    if image_frame is None:
+        image_frame = cv2.imread(image_name)
+
+    results = detector.detect_face(image_frame)
+    if results is not None:
+        total_boxes = results[0][0].astype(np.int)
+        points = results[1][0].astype(np.int)
+
+        # if height > args.max_size or width > args.max_size:
+        #    pass
+        # save crop
+        str_to_write = ""
+        str_to_write += "{},{},{},{},{}".format(total_boxes[0], total_boxes[1], total_boxes[2], total_boxes[3],
+                                                total_boxes[4])
+        str_to_write += "\n"
+        str_to_write += "{},{},{},{},{},{},{},{},{},{}".format(points[0], points[1], points[2], points[3], points[4],
+                                                               points[5], points[6], points[7], points[8], points[9])
+        # string_boxes = "%d\t%d" % (face_count, frame_count)
+        bbox_info_file_path = image_name.replace('.png', "_bbox_mtccnn.txt")
+        print('Write bbox info to ', bbox_info_file_path)
+        with open(bbox_info_file_path, 'w') as f:
+            f.write(str_to_write)
 
 def detect_and_align(img, desired_size=256):
     global detector
@@ -34,23 +61,7 @@ def detect_and_align(img, desired_size=256):
     # extract aligned face chips
     chips = detector.extract_image_chips(img, points, desired_size, 0.37)
     return chips[0]
-
-
-
-
-DATASET_DIR = {
-    'OULU-NPU': 'OULU-NPU/*/*.avi',  #
-    'CASIA-FASD': 'CASIA-FASD/*/*/*.avi',  #
-    'REPLAY-ATTACK': 'REPLAY-ATTACK/*/*/*.mov',  #
-    'REPLAY-ATTACK-SPOOF': 'REPLAY-ATTACK/*/*/*/*.mov',  #
-    'SIW': 'SIW/*/*/*/*.mov',
-    'ROSE-YOUTU': 'ROSE-YOUTU/*/*/*.mp4',  #
-    'MSU-MFSD': 'MSU-MFSD/scene01/*/*.mp4',
-    'MSU-MFSD2': 'MSU-MFSD/scene01/*/*.mov',
-    'CASIA-SURF-3DMASK': 'CASIA_SURF_3DMask/*/*/*.MOV',
-    'HKBU_MARs_V2': 'HKBU_MARs_V2/HKBU_MARs_V2/*/*/*.avi'
-}
-
+# ==========================================================================================================
 
 def process_one_video(input_fn):
     # get the input_fn ext_ratio
@@ -86,27 +97,10 @@ def process_one_video(input_fn):
         if frame_count > args.max_frames:
             break
 
-        # zip_helper.write_im_to_zip(zip, , frame)
         img_save_path = os.path.join(output_folder_dir,"{}.png".format(frame_count))
         cv2.imwrite(img_save_path,frame)
         print('Write image to ', img_save_path)
-        results = detector.detect_face(frame)
-        if results is not None:
-            total_boxes = results[0][0].astype(np.int)
-            points = results[1][0].astype(np.int)
-
-            #if height > args.max_size or width > args.max_size:
-            #    pass
-            # save crop
-            str_to_write = ""
-            str_to_write += "{},{},{},{},{}".format(total_boxes[0], total_boxes[1], total_boxes[2], total_boxes[3], total_boxes[4])
-            str_to_write+="\n"
-            str_to_write += "{},{},{},{},{},{},{},{},{},{}".format(points[0], points[1], points[2], points[3], points[4], points[5], points[6], points[7], points[8], points[9])
-            # string_boxes = "%d\t%d" % (face_count, frame_count)
-            bbox_info_file_path = img_save_path.replace('.png', "_bbox_mtccnn.txt")
-            print('Write bbox info to ', bbox_info_file_path)
-            with open(bbox_info_file_path, 'w') as f:
-                f.write(str_to_write)
+        detect_face_and_save_landmarks(img_save_path, frame)
 
     cap.release()
 
@@ -133,6 +127,19 @@ def main(args):
 
 
 if __name__ == "__main__":
+
+    DATASET_DIR = {
+        'OULU-NPU': 'OULU-NPU/*/*.avi',  #
+        'CASIA-FASD': 'CASIA-FASD/*/*/*.avi',  #
+        'REPLAY-ATTACK': 'REPLAY-ATTACK/*/*/*.mov',  #
+        'REPLAY-ATTACK-SPOOF': 'REPLAY-ATTACK/*/*/*/*.mov',  #
+        'SIW': 'SIW/*/*/*/*.mov',
+        'ROSE-YOUTU': 'ROSE-YOUTU/*/*/*.mp4',  #
+        'MSU-MFSD': 'MSU-MFSD/scene01/*/*.mp4',
+        'MSU-MFSD2': 'MSU-MFSD/scene01/*/*.mov',
+        'CASIA-SURF-3DMASK': 'CASIA_SURF_3DMask/*/*/*.MOV',
+        'HKBU_MARs_V2': 'HKBU_MARs_V2/HKBU_MARs_V2/*/*/*.avi'
+    }
 
     parser = argparse.ArgumentParser("Args parser for training")
     parser.add_argument("--num_workers", type=int, default=1, help="number of threads in the pools")
